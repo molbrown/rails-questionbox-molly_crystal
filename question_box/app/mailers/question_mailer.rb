8568b9461c46e242d5bebc70967ca2fd7b0e460d
Molly Brown <molly.stra@gmail.com>
2018-06-06 14:35:02 -0400

    mailer added

---
+++ question_box/app/mailers/question_mailer.rb
@@ -0,0 +1,23 @@
+class QuestionMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.question_mailer.answer.subject
+  #
+  def answer(question)
+    @question = question
+    @user = @question.user
+
+    @greeting = "Hi #{@user.username}!" 
+   
+    @here = url_for 'here.', question_url(@question.id, host: 'localhost:3000')
+    
+    mail(
+        to: @user.email,
+        from: "admin@codeinterview.com",
+        subject: "You've Got Answers"
+      )
+  end
+
+end
---
+++ question_box/test/mailers/previews/question_mailer_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/question_mailer
+class QuestionMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/question_mailer/answer
+  def answer
+    QuestionMailer.answer(Question.first)
+  end
+
+end
