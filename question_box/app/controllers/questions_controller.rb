1b6d06f00a4a6f17d04c57c62a0d261a5c8c8873
Molly Brown <molly.stra@gmail.com>
2018-06-06 15:08:53 -0400

    pagination gem added and implemented in index view

--- question_box/app/controllers/questions_controller.rb
+++ question_box/app/controllers/questions_controller.rb
@@ -4,7 +4,7 @@ class QuestionsController < ApplicationController
   # GET /questions
   # GET /questions.json
   def index
-    @questions = Question.all
+    @questions = Question.all.page params[:page]
   end
 
   # GET /questions/1
--- question_box/app/models/question.rb
+++ question_box/app/models/question.rb
@@ -2,6 +2,8 @@ class Question < ApplicationRecord
     belongs_to :user
     has_many :answers, dependent: :destroy
 
+    paginates_per 5
+
     validates :title, presence: true
     validates :body, presence: true 
 
