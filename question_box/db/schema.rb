b1be57d2b5f8612e1718b986315ffe071467f63c
Molly Brown <molly.stra@gmail.com>
2018-06-07 00:07:41 -0400

    faker data replaced. formatting updated to fix some truncate markdown hiccups

--- question_box/db/schema.rb
+++ question_box/db/schema.rb
@@ -60,7 +60,6 @@ ActiveRecord::Schema.define(version: 2018_06_06_200307) do
     t.boolean "admin", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email"
     t.string "email_address"
     t.index ["api_token"], name: "index_users_on_api_token", unique: true
   end
