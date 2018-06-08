1f2391a798960de5b84f5375a4c8b4ca8636aab3
Molly Brown <molly.stra@gmail.com>
2018-06-08 14:59:15 -0400

    CORS enabled

--- question_box/config/application.rb
+++ question_box/config/application.rb
@@ -12,6 +12,13 @@ module QuestionBox
     config.load_defaults 5.2
     config.serve_statis_assets = true
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
