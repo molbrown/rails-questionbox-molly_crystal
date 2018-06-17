b88bb69838a5d89bfe1ad8c07a687c1ddc5359aa
Molly Brown <molly.stra@gmail.com>
2018-06-17 12:37:31 -0400

    mailer tested

--- question_box/app/jobs/weekly_summary_job.rb
+++ question_box/app/jobs/weekly_summary_job.rb
@@ -1,8 +1,11 @@
 class WeeklySummaryJob < ActiveJob::Base
   queue_as :default
 
-  def perform(user_id)
-    user = User.find(user_id)
-    UserMailer.weekly_summary(user).deliver_now
+  def perform
+    users = User.all
+    
+    users.each do |user|
+      UserMailer.weekly_summary(user).deliver_now 
+    end
   end
 end
--- question_box/spec/routing/application_routing_spec.rb
+++ question_box/spec/routing/application_routing_spec.rb
@@ -15,16 +15,6 @@ RSpec.describe ApplicationController, type: :routing do
       expect(:get => "/logout").to route_to("logins#destroy")
     end
 
-#     it "routes to user log in" do
-#       expect(:get => "/login").to route_to("devise/sessions#new")
-#     end
 
-#     it "routes to user edit" do
-#       expect(:get => "/settings").to route_to("devise/registrations#edit")
-#     end
-
-#     it "routes to user logout" do
-#       expect(:delete => "/logout").to route_to("devise/sessions#destroy")
-#     end
   end
 end
